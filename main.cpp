6c1bc647739e7160424b91138b4855327f555f7f
M3fju <42898365+M3fju@users.noreply.github.com>
2022-04-05 23:26:58 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// Hello World
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Hello World!!!" << endl;
+    cout << "This World Is Cruel" << endl;
+
+    return 0;
+}
